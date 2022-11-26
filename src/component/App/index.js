45b0913659b965b053261291d87ce2bb3964d300
Niño Arenas <ntb.arenas@protonmail.com>
2022-11-26 21:49:39 +0000

    update style

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -10,7 +10,7 @@ const cards = [
   {
     imgUrl: "https://images.unsplash.com/photo-1559163499-413811fb2344?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     title: "Apple Macbook",
-    desc: "Dell Inspiron is a Windows 10 laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels. It is powered by a APU Quad Core A6 processor and it comes with 8GB of RAM. The Dell Inspiron packs 1TB of HDD storage. Graphics are powered by Nvidia GeForce GTX 1050.",
+    desc: "The MacBook is a brand of Mac notebook computers designed and marketed by Apple Inc. that use Apple's macOS operating system since 2006. It replaced the PowerBook and iBook brands during the Mac transition to Intel processors, announced in 2005.",
   },
   {
     imgUrl: "https://images.unsplash.com/photo-1565221287653-9a2713dbe4ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
